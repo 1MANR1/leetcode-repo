67b8badf781b46625aaccd1b4d146183146325f7
Iman <imanri2003@gmail.com>
2022-09-10 22:07:24 +0430

    Two Sum solution

---
+++ two_sum.py
@@ -0,0 +1,28 @@
+def twoSum(nums, target):
+    sum = 0
+    i = 0
+    sum_digets = []
+    active = True
+    while active:
+        range_list = len(nums) - 1
+        for i in range(range_list):
+            sum = nums[i] + nums[i+1]
+            if (sum == target):
+                sum_digets.append(i)
+                sum_digets.append(i+1)
+                active = False
+            else:
+                sum = nums[i+1]
+                list_dialects = []
+
+    return sum_digets
+
+
+test_1 = twoSum([2,7, 11, 15], 18)
+print(test_1)
+                 
+        
+
+
+    
+        
