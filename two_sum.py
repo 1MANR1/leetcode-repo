e1adee2a48aad865b42255c90e042e3de35c2b0a
Iman <imanri2003@gmail.com>
2022-09-12 10:22:58 +0430

    two sum update again

--- two_sum.py
+++ two_sum.py
@@ -1,40 +1,30 @@
-class two_sum:
-    """A simple attemt to sum two numbers in a list with a target."""
-    def __init__(self, list, target):
+class Solution:    
+    def twoSum(self, list, target):
         self.list = list
         self.target = target
         self.answer = []
         self.sum = 0
         self.i = 0
-        self.my_range = len(self.list)
-        self.k = 0
-    
-    def two_sum_fun(self):
+        self.my_range = len(self.list) - 1
+        self.k = 1
         j = 1
         active = True
         while active:
             for self.i in range(len(self.list)):
                 self.sum += self.list[self.i]
                 for self.k in range(self.my_range):
-                    self.sum += self.list[self.k]
-                    if self.sum == self.target:
-                        self.answer.append(self.k)
-                        self.answer.append(self.i)
-                        active = False
-                        return self.answer
-                    else:
-                        self.sum = self.list[self.i]
-                        j += 1
-                        print(j)
-                        if j > 10**4:
-                            return "Cannot search more"
+                        self.sum += self.list[self.k]
+                        if self.sum == self.target:
+                            if self.k != self.i:
+                                self.answer.append(self.k)
+                                self.answer.append(self.i)
+                                active = False
+                                return self.answer[::-1]
+                        else:
+                            self.sum = self.list[self.i]
+                            j += 1
                 else:
-                    self.sum = 0        
-                
-                            
-my_test = two_sum([2,7,11,15], 22)
-print(my_test.two_sum_fun())
-
+                    self.sum = 0      
 
         
 
